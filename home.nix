f608ad9a207b4bffb0b1ee826a7de37c069ebbd0
testanick <testanick@gmail.com>
2026-02-25 21:07:38 -0500

    added hyprland to home manager

--- home.nix
+++ home.nix
@@ -13,6 +13,12 @@
     };
   };
 
+  home.file.".config/hypr/custom/keybinds.conf".source = ./hypr-custom/keybinds.conf;
+  home.file.".config/hypr/custom/general.conf".source = ./hypr-custom/general.conf;
+  home.file.".config/hypr/custom/execs.conf".source = ./hypr-custom/execs.conf;
+  home.file.".config/hypr/custom/rules.conf".source = ./hypr-custom/rules.conf;
+  home.file.".config/hypr/custom/env.conf".source = ./hypr-custom/env.conf;
+
   home.packages = with pkgs; [
     firefox
     spotify
