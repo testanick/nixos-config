c4e6681b9da638e90df8ec3ab282bb3be1e09dfd
testanick <testanick@gmail.com>
2026-02-25 21:10:35 -0500

    removed illogical-impulse issues from home.nix

--- home.nix
+++ home.nix
@@ -13,11 +13,7 @@
     };
   };
 
-  home.file.".config/hypr/custom/keybinds.conf".source = ./hypr-custom/keybinds.conf;
-  home.file.".config/hypr/custom/general.conf".source = ./hypr-custom/general.conf;
-  home.file.".config/hypr/custom/execs.conf".source = ./hypr-custom/execs.conf;
-  home.file.".config/hypr/custom/rules.conf".source = ./hypr-custom/rules.conf;
-  home.file.".config/hypr/custom/env.conf".source = ./hypr-custom/env.conf;
+  home.file.".config/illogical-impulse/config.json".source = ./illogical-impulse-config.json;
 
   home.packages = with pkgs; [
     firefox
