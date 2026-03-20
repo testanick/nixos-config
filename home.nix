85016f47df5cbc2c3cd4740a9e6a5f58af7208b9
testanick <testanick@gmail.com>
2026-03-19 23:02:11 -0400

    added vim bindings to fish

--- home.nix
+++ home.nix
@@ -12,7 +12,16 @@
       starship.enable = true;
     };
   };
-
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      fish_vi_key_bindings
+      set fish_cursor_default block
+      set fish_cursor_insert line
+      set fish_cursor_replace_one underscore
+      set fish_cursor_visual block
+    '';
+  };
   home.file.".config/illogical-impulse/config.json".source = ./illogical-impulse-config.json;
 
   home.packages = with pkgs; [
